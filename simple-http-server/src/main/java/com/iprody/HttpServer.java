d6be6a7d7f16b73ed6ac92958f30d8bbd8b10159
Yury <programmerfromearth@gmail.com>
2026-01-17 14:34:23 +0300

    Lesson 1. Homework

--- simple-http-server/src/main/java/com/iprody/HttpServer.java
+++ simple-http-server/src/main/java/com/iprody/HttpServer.java
@@ -1,35 +1,140 @@
 package com.iprody;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
+import static com.iprody.utils.ContentUtils.*;
+
+@Slf4j
 public class HttpServer {
+    private static final int PORT = 8080;
+    private static final String CRLF = "\r\n";
+    private static final String ROOT_PATH = "/";
+    private static final String STATUS_LINE_TEMPLATE = "HTTP/1.1 %s";
 
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(8080);
-        System.out.println("Server started at http://localhost:8080");
-        while (true) {
-            Socket clientSocket = serverSocket.accept();
-            BufferedReader in = new BufferedReader(new
-                    InputStreamReader(clientSocket.getInputStream()));
-            BufferedWriter out = new BufferedWriter(new
-                    OutputStreamWriter(clientSocket.getOutputStream()));
+    private static String BASE_FILE_FOLDER;
 
-            // Чтение запроса
-            String line;
-            while ((line = in.readLine()) != null && !line.isEmpty()) {
-                System.out.println(line);
+    static void main(String[] args) throws IOException {
+        setUpBaseFileFolder(args);
+
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            log.info("Server started at http://localhost:" + PORT);
+            while (true) {
+                try (Socket clientSocket = serverSocket.accept()) {
+                    List<String> requestLines = readHttpRequestHeaders(clientSocket);
+                    writeHttpRequestHeadersToConsole(requestLines);
+                    sendHttpResponse(clientSocket, requestLines);
+                }
             }
-            // Простой ответ
-            String response = "<h1>Hello from server!</h1>";
-            out.write("HTTP/1.1 200 OK\r\n");
-            out.write("Content-Type: text/html; charset=UTF-8\r\n");
-            out.write("Content-Length: " + response.length() + "\r\n");
-            out.write("\r\n");
-            out.write(response);
-            out.flush();
-            clientSocket.close();
         }
+    }
+
+    private static void setUpBaseFileFolder(String[] args) {
+        if (args == null || args.length == 0) {
+            throw new IllegalArgumentException("args can not be empty");
+        }
+
+        log.info("base file folder will be setting up with first argument from command line");
+
+        BASE_FILE_FOLDER = args[0];
+    }
+
+    private static List<String> readHttpRequestHeaders(Socket socket) throws IOException {
+        BufferedReader in =
+                new BufferedReader(
+                        new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+
+        List<String> requestLines = new ArrayList<>();
+
+        String line;
+        while ((line = in.readLine()) != null && !line.isEmpty()) {
+            requestLines.add(line);
+        }
+
+        return Collections.unmodifiableList(requestLines);
+    }
+
+    private static void writeHttpRequestHeadersToConsole(List<String> requestLines) {
+        requestLines.forEach(log::info);
+    }
+
+    private static void sendHttpResponse(Socket socket, List<String> requestLines) throws IOException {
+        final Optional<String> pathOpt = extractLocationAfterServerUrl(requestLines);
+
+        if (pathOpt.isEmpty()) {
+            sendNotFoundResponse(socket);
+            return;
+        }
+
+        final String path = pathOpt.get();
+
+        if (path.equals(ROOT_PATH)) {
+            sendRootContent(socket);
+            return;
+        }
+
+        final Path absolutePath = Path.of(BASE_FILE_FOLDER, path).toAbsolutePath();
+
+        if (!isValidFile(absolutePath)) {
+            sendNotFoundResponse(socket);
+            return;
+        }
+
+        sendFileContent(socket, absolutePath);
+    }
+
+    private static void sendRootContent(Socket socket) throws IOException {
+        String response = "<html><body><h1>Hello from server!</h1></body></html>";
+        writeHttpResponse(
+                socket,
+                STATUS_LINE_TEMPLATE.formatted("200 OK"),
+                "text/html",
+                response.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static void sendFileContent(Socket socket, Path filePath) throws IOException {
+        final byte[] content = Files.readAllBytes(filePath);
+        final String contentType = defineContentType(filePath);
+        writeHttpResponse(
+                socket,
+                STATUS_LINE_TEMPLATE.formatted("200 OK"),
+                contentType,
+                content);
+    }
+
+    private static void sendNotFoundResponse(Socket socket) throws IOException {
+        String response = "<html><body><h1>404 Not Found</h1></body></html>";
+        writeHttpResponse(
+                socket,
+                STATUS_LINE_TEMPLATE.formatted("404 Not Found"),
+                "text/html",
+                response.getBytes(StandardCharsets.UTF_8));
+    }
+
+    private static void writeHttpResponse(Socket socket,
+                                          String statusLine,
+                                          String contentType,
+                                          byte[] responseBytes) throws IOException {
+        BufferedWriter out =
+                new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+
+        out.write(statusLine + CRLF);
+        out.write("Content-Type: %s; charset=UTF-8".formatted(contentType) + CRLF);
+        out.write("Content-Length: " + responseBytes.length + CRLF);
+        out.write(CRLF);
+        out.write(new String(responseBytes, StandardCharsets.UTF_8));
+
+        out.flush();
     }
 }
