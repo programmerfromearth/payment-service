3f5f37be56ba9e3088e420bbf5cdd07796541837
Yury <programmerfromearth@gmail.com>
2026-02-01 18:48:57 +0300

    Lesson 6. Add unit tests

---
+++ payment-service-app/src/test/java/com/iprody/payment/service/app/service/payment/impl/PaymentServiceImplTest.java
@@ -0,0 +1,75 @@
+package com.iprody.payment.service.app.service.payment.impl;
+
+import com.iprody.payment.service.app.persistency.entity.PaymentEntity;
+import com.iprody.payment.service.app.persistency.repository.PaymentRepository;
+import com.iprody.payment.service.app.service.converter.PaymentConverter;
+import com.iprody.payment.service.app.service.payment.model.Payment;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class PaymentServiceImplTest {
+
+    @Mock
+    private PaymentRepository paymentRepository;
+
+    @Mock
+    private PaymentConverter paymentConverter;
+
+    @InjectMocks
+    private PaymentServiceImpl paymentService;
+
+    @Test
+    void whenPaymentsExistThenReturnMappedList() {
+        PaymentEntity entity = new PaymentEntity();
+        Payment model = Payment.builder().guid(UUID.randomUUID()).build();
+
+        when(paymentRepository.findAll()).thenReturn(List.of(entity));
+        when(paymentConverter.toModel(entity)).thenReturn(model);
+
+        List<Payment> result = paymentService.getAllPayments();
+
+        assertThat(result).hasSize(1);
+        assertThat(result.getFirst()).isEqualTo(model);
+        verify(paymentRepository).findAll();
+        verify(paymentConverter).toModel(any());
+    }
+
+    @Test
+    void whenIdExistsThenReturnMappedPayment() {
+        UUID id = UUID.randomUUID();
+
+        PaymentEntity entity = new PaymentEntity();
+        Payment model = Payment.builder().guid(id).build();
+
+        when(paymentRepository.findById(id)).thenReturn(Optional.of(entity));
+        when(paymentConverter.toModel(entity)).thenReturn(model);
+
+        Optional<Payment> result = paymentService.findPaymentById(id);
+
+        assertThat(result).isPresent().contains(model);
+    }
+
+    @Test
+    void whenIdNotExistsThenReturnEmpty() {
+        UUID id = UUID.randomUUID();
+
+        when(paymentRepository.findById(id)).thenReturn(Optional.empty());
+
+        Optional<Payment> result = paymentService.findPaymentById(id);
+
+        assertThat(result).isEmpty();
+        verifyNoInteractions(paymentConverter);
+    }
+}
