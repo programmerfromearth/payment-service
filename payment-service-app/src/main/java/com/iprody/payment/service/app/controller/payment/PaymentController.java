4b7e4091b5b735a93bcac2a910484192e62ab85c
Yury <programmerfromearth@gmail.com>
2026-03-07 13:11:29 +0300

    homework-15. add logging. Add actuator

--- payment-service-app/src/main/java/com/iprody/payment/service/app/controller/payment/PaymentController.java
+++ payment-service-app/src/main/java/com/iprody/payment/service/app/controller/payment/PaymentController.java
@@ -9,6 +9,7 @@ import com.iprody.payment.service.app.service.payment.api.PaymentService;
 import com.iprody.payment.service.app.service.payment.model.PaymentDto;
 import com.iprody.payment.service.app.service.payment.model.PaymentFilter;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
@@ -32,6 +33,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @RestController
 @RequestMapping("/payments")
 @AllArgsConstructor
@@ -42,6 +44,7 @@ public class PaymentController {
     @GetMapping
     @PreAuthorize("hasAnyRole('admin', 'reader')")
     public List<PaymentResponse> getAllPayments() {
+        log.info("GET all payments");
         return paymentService.getAllPayments().stream()
                 .map(paymentMapper::toApiResponse)
                 .toList();
@@ -51,6 +54,7 @@ public class PaymentController {
     @PreAuthorize("hasAnyRole('admin', 'reader')")
     public Page<PaymentResponse> searchPagedByFilter(@ModelAttribute PaymentFilterRequest paymentFilterRequest,
                                                      @PageableDefault(page = 0, size = 25) Pageable pageable) {
+        log.info("GET all payments by search filter: {}", paymentFilterRequest);
         final PaymentFilter paymentFilter = paymentMapper.toPaymentFilter(paymentFilterRequest);
         return paymentService.searchPagedByFilter(paymentFilter, pageable)
                 .map(paymentMapper::toApiResponse);
@@ -59,19 +63,22 @@ public class PaymentController {
     @GetMapping("/{id}")
     @PreAuthorize("hasAnyRole('admin', 'reader')")
     public PaymentResponse getPaymentById(@PathVariable("id") UUID id) {
+        log.info("GET payment by id: {}", id);
         final PaymentDto result = paymentService.getById(id);
-
+        log.debug("Sending response PaymentDto: {}", result);
         return paymentMapper.toApiResponse(result);
     }
 
     @PostMapping
     @PreAuthorize("hasRole('admin')")
     public ResponseEntity<PaymentResponse> create(@RequestBody PaymentToCreateRequest toCreateRequest) {
+        log.info("POST create payment: {}", toCreateRequest);
         final PaymentDto toCreateDto = paymentMapper.toDto(toCreateRequest);
 
         final PaymentDto result = paymentService.create(toCreateDto);
 
         final PaymentResponse resultMapped = paymentMapper.toApiResponse(result);
+        log.debug("Sending response PaymentDto: {}", result);
 
         final URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -88,8 +95,10 @@ public class PaymentController {
     @PreAuthorize("hasRole('admin')")
     public PaymentResponse update(@PathVariable("id") UUID id,
                                   @RequestBody PaymentToCreateRequest toCreateRequest) {
+        log.info("PUT update payment by id: {}, {}", id, toCreateRequest);
         final PaymentDto toCreateDto = paymentMapper.toDto(toCreateRequest);
         final PaymentDto result = paymentService.update(id, toCreateDto);
+        log.debug("Sending response PaymentDto: {}", result);
 
         return paymentMapper.toApiResponse(result);
     }
@@ -98,8 +107,9 @@ public class PaymentController {
     @PreAuthorize("hasRole('admin')")
     public PaymentResponse updatePart(@PathVariable("id") UUID id,
                                       @RequestBody PaymentToPartUpdateRequest toPartUpdateRequest) {
+        log.info("PATCH partial update payment by id: {}, {}", id, toPartUpdateRequest);
         final PaymentDto result = paymentService.updateNote(id, toPartUpdateRequest);
-
+        log.debug("Sending response PaymentDto: {}", result);
         return paymentMapper.toApiResponse(result);
     }
 
@@ -107,6 +117,7 @@ public class PaymentController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PreAuthorize("hasRole('admin')")
     public void deleteById(@PathVariable("id") UUID id) {
+        log.info("DELETE delete payment by id: {}", id);
         paymentService.deleteById(id);
     }
 }
